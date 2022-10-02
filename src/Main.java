ed4145b619de4cef28b56d04e3173cf99f27cffa
Станислав <zurlov.s.s@outlook.com>
2022-10-02 22:40:47 +0300

    Work home 7. Строки. Домашняя работа, выполнена.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task 1.
+        String firstName = "ivanov";
+        String middleName = "ivan";
+        String lastName = "ivanovich";
+        String fullName = "ivanov ivan ivanovich";
+        String iii = "ФИО сотрудника -  " + firstName + ' ' +  middleName + ' ' + lastName;
+        System.out.println(iii);
+
+        System.out.println();
+        //task 2.
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName);
+
+        System.out.println();
+        //task 3.
+        String russFullName = "Иванов Семён Семёнович";
+        russFullName = russFullName.replace("ё","е");
+        System.out.println(russFullName);
+
     }
 }
